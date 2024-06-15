6b22384c59615d1a97b992e315e957376a3ba55b
yunus.sen <yunus.sen@n11.com>
2024-06-15 08:18:53 +0300

    added valueObject and base entity to common domain module.

---
+++ common/common-doamin/src/main/java/com/food/ordering/system/domain/enitity/AggregateRoot.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.domain.enitity;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
+}
---
+++ common/common-doamin/src/main/java/com/food/ordering/system/domain/valueobject/ResturantId.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.util.UUID;
+
+public class ResturantId extends BaseId<UUID>{
+    public ResturantId(UUID value) {
+        super(value);
+    }
+}
