bd05f74698b003169450db2dc05c07db3ef552a3
yunus.sen <yunus.sen@n11.com>
2024-06-18 11:23:56 +0300

    implementing order domain service.

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entiity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entiity/Product.java
@@ -4,11 +4,11 @@ import com.food.ordering.system.domain.enitity.BaseEntity;
 import com.food.ordering.system.domain.valueobject.Money;
 import com.food.ordering.system.domain.valueobject.ProductId;
 
-public class Product extends BaseEntity<ProductId>{
+public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
 
-    public Product(ProductId productId,String name, Money price) {
+    public Product(ProductId productId, String name, Money price) {
         super.setId(productId);
         this.name = name;
         this.price = price;
@@ -20,5 +20,10 @@ public class Product extends BaseEntity<ProductId>{
 
     public Money getPrice() {
         return price;
+    }
+
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
     }
 }
